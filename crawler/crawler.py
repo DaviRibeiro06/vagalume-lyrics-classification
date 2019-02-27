91e7d451a2d68051320068f607e5a73bb37c8f52
Raul de Araújo Lima <rauldearaujolima@gmail.com>
2019-02-27 16:21:55 -0300

    Version 1.0

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,24 +1,23 @@
 import scrapy
 import pandas as pd
+from langdetect import detect
+
+dataset = {
+    'artist_name': [],
+    'music_name': [],
+    'genre': [],
+    'music_lyric': [],
+    'language': []
+}
 
 class VagalumeCrawler(scrapy.Spider):
-    
-    dataset = {
-        'artist_name': [],
-        'music_name': [],
-        'genre': [],
-        'music_lyric': [],
-        'language': []
-    }
-
     name = 'vagalume'
     start_urls = [
         'https://www.vagalume.com.br/browse/style/axe.html',
-        'https://www.vagalume.com.br/browse/style/bossa-nova.html',
-        'https://www.vagalume.com.br/browse/style/forro.html'
         ]
     
     def parse(self, response):
+        global dataset
         artists_urls = []
         genre = response.url.split('/')[-1].split('.')[0]
         for column in response.css('.namesColumn'):
@@ -33,26 +32,26 @@ class VagalumeCrawler(scrapy.Spider):
         pd.DataFrame(artist).to_csv('artists_{}.csv'.format(genre), index=False)
         '''        
         for artist_url in artists_urls:
-            yield response.follow(url=artist_url, callback=self.parser_artist)
+            yield response.follow(url=artist_url, callback=self.parser_artist, meta={'genre':genre})
 
-        pd.DataFrame(VagalumeCrawler.dataset).to_csv('vagalume.csv', index=False)
+        pd.DataFrame(dataset).to_csv('vagalume.csv', index=False)
 
     def parser_artist(self, response):
         artist_url = response.url
         musics_urls = []
         for item_music in response.css('#alfabetMusicList li .flexSpcBet .lineColLeft'):
-            musics_urls.append(item_music.css('a::attr(href)'))
+            musics_urls.append(item_music.css('a::attr(href)').extract_first())
 
         for music_url in musics_urls:
-            yield response.follow(url=music_url, callback=self.parse_music)
+            yield response.follow(url=music_url, callback=self.parse_music, meta={'genre':response.meta['genre']})
     
     def parse_music(self, response):
-        VagalumeCrawler.dataset['artist_name'].append(response.css('.col1-2-1 h2::text').extract_first())
-        VagalumeCrawler.dataset['music_name'].append(response.css('.col1-2-1 h1::text').extract_first())
-        VagalumeCrawler.dataset['music_lyric'].append(response.css('.col1-2-1 #lyrics::text').extract_first())
-        #VagalumeCrawler.dataset['genre'].append(response.meta['genre'])
-        VagalumeCrawler.dataset['genre'].append(None)
-        VagalumeCrawler.dataset['language'].append(None)
+        global dataset
+        dataset['artist_name'].append(response.css('.col1-2-1 h2::text').extract_first())
+        dataset['music_name'].append(response.css('.col1-2-1 h1::text').extract_first())
+        dataset['music_lyric'].append('\n'.join(response.css('.col1-2-1 #lyrics::text').extract()))
+        dataset['genre'].append(response.meta['genre'])
+        dataset['language'] = detect(' '.join(response.css('.col1-2-1 #lyrics::text').extract()))
 
         
 
